5c51abd0f929911efa50992e30b3b40d785fcde4
Raycoms <ray.neiheiser@gmail.com>
2025-07-28 12:46:17 +0200

    fixes

--- src/main/java/com/minecolonies/core/colony/events/raid/egyptianevent/EgyptianRaidEvent.java
+++ src/main/java/com/minecolonies/core/colony/events/raid/egyptianevent/EgyptianRaidEvent.java
@@ -15,6 +15,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -53,7 +54,7 @@ public class EgyptianRaidEvent extends HordeRaidEvent
     public void onStart()
     {
         super.onStart();
-        PlayAudioMessage.sendToAll(getColony(), true, false, new PlayAudioMessage(RaidSounds.DESERT_RAID_WARNING));
+        PlayAudioMessage.sendToAll(getColony(), true, false, new PlayAudioMessage(RaidSounds.DESERT_RAID_WARNING, SoundSource.HOSTILE));
     }
 
     @Override
@@ -70,7 +71,7 @@ public class EgyptianRaidEvent extends HordeRaidEvent
 
         if (--musicCooldown <= 0)
         {
-            PlayAudioMessage.sendToAll(getColony(), true, true, new PlayAudioMessage(RaidSounds.DESERT_RAID));
+            PlayAudioMessage.sendToAll(getColony(), true, true, new PlayAudioMessage(RaidSounds.DESERT_RAID, SoundSource.HOSTILE));
             musicCooldown = 12;
         }
     }
